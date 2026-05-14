43d13462b99b17e05f5354bde9f13001d13bfccf
Carlm832 <carltonnjenga@gmail.com>
2026-05-14 15:57:16 +0300

    Add environment debug info to test-db.php

--- api/test-db.php
+++ api/test-db.php
@@ -9,6 +9,16 @@ header('Content-Type: text/plain');
 echo "--- LOUD DEBUG MODE ---\n";
 echo "Checking environment variables...\n";
 
+// Debug info
+echo "<h3>Environment Debug</h3>";
+echo "DB_TYPE: " . (getenv('DB_TYPE') ?: 'NOT SET (defaulting to mysql)') . "<br>";
+echo "DB_HOST: " . (getenv('DB_HOST') ?: 'NOT SET (defaulting to localhost)') . "<br>";
+echo "DB_PORT: " . (getenv('DB_PORT') ?: 'NOT SET') . "<br>";
+echo "DB_NAME: " . (getenv('DB_NAME') ?: 'NOT SET') . "<br>";
+echo "DB_USER: " . (getenv('DB_USER') ?: 'NOT SET') . "<br>";
+echo "VERCEL: " . (getenv('VERCEL') ?: 'NOT SET') . "<br>";
+echo "<hr>";
+
 $type = getenv('DB_TYPE') ?: 'mysql';
 $host = getenv('DB_HOST');
 $port = getenv('DB_PORT');
